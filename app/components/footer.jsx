41dfd5a82caa8f480f9062a66b6acf92bc553399
muzammilpro <muzammilhusnain7@gmail.com>
2024-04-13 23:26:23 +0500

    My Portfolio

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://instagram.com/devinsightmuz" className="text-[#16f2b3]">Muzammil Husnain</Link>
+            © Developer Portfolio of <Link target="_blank" href="https://instagram.com/devinsightmuz" className="text-[#16f2b3]">Muzammil Husnain</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
