0087df120d481048397263028624d14e6d6b4807
muzammilpro <muzammilhusnain7@gmail.com>
2024-04-13 23:24:01 +0500

    My Portfolio

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,41 +1,117 @@
+"use client"
 // @flow strict
-import Link from "next/link";
-
+import { useState } from 'react';
+import Link from 'next/link';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
-        <div className="flex flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
+        <div className="flex items-center">
+          <Link href="/" className="text-[#16f2b3] text-3xl font-bold">
             Muzammil Husnain
           </Link>
         </div>
 
-        <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100" id="navbar-default">
+        <div className="md:hidden">
+          <button
+            onClick={toggleNavbar}
+            type="button"
+            className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              {isOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 5h16v2H4V5zm0 6h16v2H4v-2zm0 6h16v2H4v-2z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4 6h16v2H4V6zm0 7h16v2H4v-2zm0 7h16v2H4v-2z"
+                />
+              )}
+            </svg>
+          </button>
+        </div>
+
+        <ul
+          className={`${
+            isOpen ? 'block' : 'hidden'
+          } md:flex mt-4 md:mt-0 md:flex-row md:space-x-1 md:border-0`}
+          id="navbar-default"
+        >
           <li>
-            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#about">
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="#about"
+              onClick={toggleNavbar}
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                ABOUT
+              </div>
             </a>
           </li>
           <li>
-            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></a>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="#experience"
+              onClick={toggleNavbar}
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                EXPERIENCE
+              </div>
+            </a>
           </li>
           <li>
-            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></a>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="#skills"
+              onClick={toggleNavbar}
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                SKILLS
+              </div>
+            </a>
           </li>
           <li>
-            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></a>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="#education"
+              onClick={toggleNavbar}
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                EDUCATION
+              </div>
+            </a>
           </li>
           <li>
-            <a className="block px-4 py-2 no-underline outline-none hover:no-underline" href="#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></a>
+            <a
+              className="block px-4 py-2 no-underline outline-none hover:no-underline"
+              href="#projects"
+              onClick={toggleNavbar}
+            >
+              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                PROJECTS
+              </div>
+            </a>
           </li>
         </ul>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
